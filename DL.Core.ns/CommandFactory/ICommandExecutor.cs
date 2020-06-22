602330b2bb3195c6e5dbd8bd1ee69acc7b47647c
xuguilins <oncwnuFN50RNIp4CrY7ZpfgW-f_s@git.weixin.qq.com>
2020-06-22 09:26:18 +0800

    增加命令模块包

--- DL.Core.ns/CommandFactory/ICommandExecutor.cs
+++ DL.Core.ns/CommandFactory/ICommandExecutor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DL.Core.ns.Dependency;
 
 namespace DL.Core.ns.CommandFactory
 {
---
+++ DL.Core.ns/CorePack/CommandPack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using DL.Core.ns.CommandFactory;
+
+namespace DL.Core.ns.CorePack
+{
+    public class CommandPack : PackBase
+    {
+        public override int StarLevel => 40;
+
+        public override IServiceCollection AddService(IServiceCollection services)
+        {
+            services.AddScoped<ICommandExecutor, CommandExecutor>();
+            return services;
+        }
+    }
+}
